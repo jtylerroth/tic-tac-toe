e43ad73312c10cf2e981347e35e79d97bca1e2e8
tothjmt <toth.jmt@gmail.com>
2016-05-06 21:56:55 -0400

    UI and totals

--- app.js
+++ app.js
@@ -16,22 +16,25 @@ var x_spots;
 var o_spots;
 var gameOn;
 
-function startGame(firstPlayer) {
+var winnings = { x: [], o: [] };
+
+function startGame(firstPlayer, table) {
     //Set blank game board
     current = {
+	    started: firstPlayer,
         player: firstPlayer,
-        turn: 1
+        turn: 1,
+	    table: table
     };
+
+	table.find('.first-player').text(firstPlayer.toUpperCase());
+
     x_spots = [];
     o_spots = [];
     gameOn = true;
-    render();
     console.log('New match started. ' + firstPlayer + ' goes first.');
 
     while (gameOn) {
-        console.log(current);
-        console.log(x_spots);
-        console.log(o_spots);
         takeTurn();
     }
 }
@@ -42,46 +45,78 @@ function takeTurn() {
         console.log('TIE');
         return;
     }
-    if (current.player == 'x') {
-        x_spots.push(x_botTurn());
+
+	var spot = 0;
+    if (current.player == 'x')
+    {
+	    spot = x_botTurn();
+        x_spots.push(spot);
     }
-    else {
-        o_spots.push(o_botTurn());
+    else
+    {
+	    spot = o_botTurn();
+        o_spots.push(spot);
     }
 
-    if (current.turn > 5) {
-        if(checkForWin())
+	//console.log(spot + ': ' + current.player.toUpperCase());
+	current.table.find('.board-' + spot).text(current.player.toUpperCase());
+
+    if (current.turn > 5)
+    {
+	    var won = checkForWin();
+        if(won)
         {
-            console.log(current.player + " wins!")
+	        for(var w = 0; w < won.length; w++)
+	        {
+		        current.table.find('.board-' + won[w]).addClass('winning');
+	        }
            gameOn = false;
+
+	        var winner = current.player;
+	        winnings[winner].push(current.started);
         }
     }
-    render();
+
     current.player = current.player == 'x' ? 'o' : 'x';
     current.turn++;
 }
 
 //Tyler
 function x_botTurn() {
-    // var random = getRandom();
-    //
-    // while (x_spots.indexOf(random) > -1 || o_spots.indexOf(random) > -1) {
-    //     random = getRandom()
-    // }
-
-
-    // return random;
-}
-//Josh
-function o_botTurn() {
     var random = getRandom();
 
     while (x_spots.indexOf(random) > -1 || o_spots.indexOf(random) > -1) {
-        random = getRandom()
+         random = getRandom()
+    }
+
+
+    return random;
+}
+
+
+//Josh
+function o_botTurn()
+{
+
+	/*
+		Ref:
+		1  |  2  |  3
+		4  |  5  |  6
+		7  |  8  |  9
+	 */
+
+	
+	var random = getRandom();
+
+	while (exists(random))
+	{
+        random = getRandom();
     }
 
     return random;
 }
+
+
 
 function checkForWin() {
     var checkSpots;
@@ -99,7 +134,6 @@ function checkForWin() {
     var matchedCount = 0;
 
     // console.info('CHECKING ' + current.player);
-    console.log(checkSpots);
     winningLoop: for(var w = 0; w < winningCombos.length; w++)
     {
         matchedCount = 0;
@@ -116,7 +150,7 @@ function checkForWin() {
 
                 if (matchedCount === 3)
                 {
-                    return true;
+                    return winningCombos[w];
                 }
             }
             else
@@ -130,10 +164,6 @@ function checkForWin() {
     return false;
 }
 
-function render() {
-    //UI render here
-}
-
 function exists(num){
     return !!(o_spots.indexOf(num) > -1 || x_spots.indexOf(num) > -1);
 }
@@ -142,5 +172,91 @@ function getRandom() {
     return Math.floor((Math.random() * 9) + 1);
 }
 
-startGame('x');
-// });
+
+$(document).ready(function()
+{
+	var container = $('#tables');
+	var tableCount = parseInt(container.data('generate'));
+
+	for(var t = 1; t <= tableCount; t++)
+	{
+		var table = $('<table>');
+		var cellCount = 1;
+
+		table.append('<tr><td colspan="3" class="first-player"></td>');
+
+		for (var trc = 1; trc <= 3; trc++)
+		{
+			var tr = $('<tr>');
+			for (var tdc = 1; tdc <= 3; tdc++)
+			{
+				tr.append('<td class="board-' + cellCount++ + '">');
+			}
+			table.append(tr);
+		}
+		container.append(table);
+	}
+
+	var lastPlayer = 'x';
+	container.find('table').each(function()
+	{
+		var player = (lastPlayer === 'x') ? 'o' : 'x';
+		lastPlayer = player;
+		startGame(player, $(this));
+	});
+
+	// Show winnings
+	var xWins = winnings.x.length;
+	var oWins = winnings.o.length;
+
+	if (xWins > oWins)
+	{
+		$('#wins .player-x').addClass('winner');
+	}
+	else if (xWins < oWins)
+	{
+		$('#wins .player-o').addClass('winner');
+	}
+
+	$('#wins .player-x td:last').text(xWins);
+	$('#wins .player-o td:last').text(oWins);
+
+	var xWinsOStarted = 0;
+	var xWinsXStarted = 0;
+	var oWinsXStarted = 0;
+	var oWinsOStarted = 0;
+
+	for(var x = 0; x < winnings.x.length; x++)
+	{
+		if (winnings.x[x] === 'x')
+		{
+			xWinsXStarted++
+		}
+		else
+		{
+			xWinsOStarted++;
+		}
+	}
+
+	for(var o = 0; o < winnings.o.length; o++)
+	{
+		if (winnings.o[o] === 'x')
+		{
+			oWinsXStarted++
+		}
+		else
+		{
+			oWinsOStarted++;
+		}
+	}
+
+	$('#wins .player-x td:eq(1)').text(xWinsXStarted);
+	$('#wins .player-x td:eq(2)').text(xWinsOStarted);
+
+	$('#wins .player-o td:eq(1)').text(oWinsXStarted);
+	$('#wins .player-o td:eq(2)').text(oWinsOStarted);
+
+
+
+
+});
